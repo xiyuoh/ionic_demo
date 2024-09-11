202bd823d0b539b571221336882039bfe4d976f4
Aaron Chong <aaronchongth@gmail.com>
2024-09-11 22:50:37 +0800

    Ac/tf listener (#1)
    * Basic TF listener, not tested yet
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Started README
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    ---------
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- fleet_adapter_nav2/fleet_adapter_nav2/robot_api.py
+++ fleet_adapter_nav2/fleet_adapter_nav2/robot_api.py
@@ -14,6 +14,12 @@
 
 import enum
 
+import rclpy
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer as TransformBuffer
+import tf_transformations
+
+
 class RobotAPIResult(enum.IntEnum):
     SUCCESS = 0
     """The request was successful"""
@@ -25,18 +31,44 @@ class RobotAPIResult(enum.IntEnum):
     """The client connected but something about the request is impossible"""
 
 
+class RobotUpdateData:
+    """Update data for a single robot."""
+
+    def __init__(self, data):
+        self.robot_name = data['robot_name']
+        position = data['position']
+        x = position['x']
+        y = position['y']
+        yaw = position['yaw']
+        self.position = [x, y, yaw]
+        self.map = data['map_name']
+        self.battery_soc = data['battery'] / 100.0
+        self.requires_replan = data.get('replan', False)
+
+        # TODO(@xiyuoh) check task completion using nav2 API inside get_data instead
+        self.last_request_completed = data['last_completed_request']
+
+    def is_command_completed(self, cmd_id):
+        return self.last_request_completed == cmd_id
+
+
 class RobotAPI:
     # The constructor below accepts parameters typically required to submit
     # http requests. Users should modify the constructor as per the
     # requirements of their robot's API
-    def __init__(self, prefix: str, user: str, password: str):
+    def __init__(self, prefix: str, tf_buffer: TransformBuffer):
         self.prefix = prefix
-        self.user = user
-        self.password = password
+        self.base_frame = f"{self.prefix}/base_footprint"
+        self.map_frame = f"{self.prefix}/map"
+        self.tf_buffer = tf_buffer
         self.timeout = 5.0
         self.debug = False
 
-        
+        # TODO(AC): Populate these fields properly via ROS 2 subscriptions and
+        # nav2 simple commander API feedback
+        self.battery_soc = 100 - 1e-9
+        self.last_completed_request = None
+
 
     def check_connection(self):
         """Return True if connection to the robot API server is successful."""
@@ -96,28 +128,38 @@ class RobotAPI:
 
         Otherwise return a list of RobotUpdateData for all robots.
         """
-        ########################################################################
-        # IMPLEMENT CODE HERE
-        ########################################################################
-        return None
+        if robot_name is not self.robot_name:
+            raise RuntimeError(
+                f"Calling get_data with robot [{robot_name}] while RobotAPI is for robot [{self.robot_name}]"
+            )
 
+        transform_stamped = None
+        try:
+            transform_stamped = self.tf_buffer.lookup_transform(
+                self.base_frame,
+                self.map_frame,
+                rclpy.time.Time())
+        except TransformException as ex:
+            self.get_logger().info(
+                f'Could not transform {self.base_frame} to {self.map_frame}: {ex}')
+            return None
 
-class RobotUpdateData:
-    """Update data for a single robot."""
-
-    def __init__(self, data):
-        self.robot_name = data['robot_name']
-        position = data['position']
-        x = position['x']
-        y = position['y']
-        yaw = position['yaw']
-        self.position = [x, y, yaw]
-        self.map = data['map_name']
-        self.battery_soc = data['battery'] / 100.0
-        self.requires_replan = data.get('replan', False)
-
-        # TODO(@xiyuoh) check task completion using nav2 API inside get_data instead
-        self.last_request_completed = data['last_completed_request']
-
-    def is_command_completed(self, cmd_id):
-        return self.last_request_completed == cmd_id
+        theta = tf_transformations.euler_from_quaternion([
+            transform_stamped.transform.rotation.x,
+            transform_stamped.transform.rotation.y,
+            transform_stamped.transform.rotation.z,
+            transform_stamped.transform.rotation.w
+        ])[2]
+        position = {
+            "x": transform_stamped.transform.translation.x,
+            "y": transform_stamped.transform.translation.y,
+            "yaw": theta,
+        }
+        data = {
+            "robot_name": self.robot_name,
+            "position": position,
+            "map_name": self.map_name,
+            "battery": self.battery_soc,
+            "last_completed_request": self.last_completed_request
+        }
+        return data
